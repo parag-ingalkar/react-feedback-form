1a7cbf9f3a27e5ee049f83c2e6f0e06ed4eb14bd
parag-ingalkar <paragingalkar@gmail.com>
2025-05-05 15:24:23 -0400

    Integrated form state and onchange event

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -3,6 +3,17 @@ import './FeedbackForm.css'; // Import CSS for styling
 
 const FeedbackForm = () => {
 
+    const [formData, setFormData] = useState({
+        name:'',
+        email:'',
+        feedback:''
+    });
+
+    const handleChange = (event) => {
+        const {name:value} = event.target;
+        setFormData({...formData, [name]:value});
+    };
+
   return (
     <>
     <nav>
@@ -11,7 +22,21 @@ const FeedbackForm = () => {
       <form className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        
+        <input type='text'
+        name='name'
+        placeholder='Your Name'
+        value={formData.name}
+        onChange={handleChange}/>
+        <input type='email'
+        name='email'
+        placeholder='Your E-mail'
+        value={formData.email}
+        onChange={handleChange}/>
+        <textarea name='feedback'
+        placeholder='Your Feedback'
+        value={formData.feedback}
+        onChange={handleChange}/>
+        <button type='submit'>Submit Feedback </button> 
       </form>
     </>
   );
